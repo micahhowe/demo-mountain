742700eb99fb6487220359fb32769cef18b3d5c9
micahhowe <micahhowe182@gmail.com>
2019-08-29 16:40:43 -0600

    added selectors

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+        <p className="fixed">Fixed</p>
         <div className="square">
           <button>Click Here</button>
+          <p id="absolute">Absolute</p>
         </div>
         <div className="floating">
           <p>Floating</p>
