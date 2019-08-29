7cc270c5ccd287d8d51d6552db9d5cd4e328aefc
micahhowe <micahhowe182@gmail.com>
2019-08-29 16:17:23 -0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <div className="square">
+          <button>Click Here</button>
+        </div>
+        <div className="floating">
+          <p>Floating</p>
+        </div>
+    </div>
+  );
+}
+
+export default App;
