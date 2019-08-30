8159646f2d6a25fb49667b6c70c52d15e16b895e
micahhowe <micahhowe182@gmail.com>
2019-08-30 14:26:54 -0600

    made progress on the demo

--- src/App.js
+++ src/App.js
@@ -4,16 +4,35 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-        <p className="fixed">Fixed</p>
+      {/* 59C */}
+      <div id="status" role="alert" aria-live="assertive" class="online">
+      <p>You are online.</p>
+    </div>
+      {/* 54H */}
+        <h1><a href="https://www.google.com">lol</a></h1>
+      {/* 54J 54E-2 54G 59E	*/}
+        <img style={{height:'40px'}} src="http://www.pngmart.com/files/5/Donuts-PNG-File.png" alt="a pink donut" />
+        <h2>Not Fixed h2</h2><span className="fixed">Fixed</span>
         <div className="square">
           <button>Click Here</button>
           <p id="absolute">Absolute</p>
         </div>
+        {/* 54D-1 */}
+        <article>Article</article>
+          <section>Section</section>
+          <footer>Footer</footer>
         <div className="floating">
-          <p>Floating</p>
+        {/* 54C-2 54E-1	 */}
+          <p><ul>
+            <li>Floating</li>
+            <li>List</li>
+            </ul></p>
         </div>
+        
     </div>
   );
 }
 
 export default App;
+
+
